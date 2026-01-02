014573bcce3bf9692e160c65e14ed6711bacfcbb
Real Salman <salmanibneakbar@gmail.com>
2026-01-02 23:13:37 +0600

    GO

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -109,12 +109,6 @@ const Home = () => {
               </h3>
               <div className="space-y-4 text-sm">
                 <div className="flex items-center justify-between">
-                  <span className="text-black dark:text-white">Fixed Cost</span>
-                  <svg className="w-6 h-6 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                  </svg>
-                </div>
-                <div className="flex items-center justify-between">
                   <span className="text-black dark:text-white">Affordable Price</span>
                   <svg className="w-6 h-6 text-green-500" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
@@ -158,12 +152,6 @@ const Home = () => {
               </div>
               <div className="space-y-4">
                 <div className="flex items-center justify-between">
-                  <span className="text-black">Fixed Cost</span>
-                  <svg className="w-6 h-6 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                  </svg>
-                </div>
-                <div className="flex items-center justify-between">
                   <span className="text-black">Affordable Price</span>
                   <svg className="w-6 h-6 text-green-500" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
@@ -202,12 +190,6 @@ const Home = () => {
                 On-Shore Devs
               </h3>
               <div className="space-y-4 text-sm">
-                <div className="flex items-center justify-between">
-                  <span className="text-black dark:text-white">Fixed Cost</span>
-                  <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                  </svg>
-                </div>
                 <div className="flex items-center justify-between">
                   <span className="text-black dark:text-white">Affordable Price</span>
                   <svg className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
